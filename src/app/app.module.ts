301992efd3b55ba1d1017faf9c95e39277c08d17
Dan Tiberi <danny.tiberi@gmail.com>
2023-03-29 17:53:26 -0500

    Integrated dark-mode foundations.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BannerComponent } from './banner/banner.component';
+import { DarkModeToggleComponent } from './banner/dark-mode-toggle/dark-mode-toggle.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     BannerComponent,
+    DarkModeToggleComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HomeModule,
     BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/banner/dark-mode-toggle/dark-mode-toggle.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { DarkModeService } from 'angular-dark-mode';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-dark-mode-toggle',
+  templateUrl: './dark-mode-toggle.component.html',
+  styleUrls: ['./dark-mode-toggle.component.scss']
+})
+export class DarkModeToggleComponent {
+  darkMode$: Observable<boolean> = this.darkModeService.darkMode$;
+  
+  constructor (private darkModeService: DarkModeService) {
+    this.darkModeService.disable;
+  }
+
+  onToggle(): void {
+    this.darkModeService.toggle();
+    console.log("WOW");
+  }
+}
