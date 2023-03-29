f37664587402112c5e7e46e123c29ddb025636fd
Dan Tiberi <danny.tiberi@gmail.com>
2023-03-29 17:35:29 -0500

    Created banner component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { HomeModule } from './pages/home/home.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    BannerComponent,
   ],
   imports: [
     BrowserModule,
