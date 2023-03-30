8c9df537c1c9ce4db75d0b674b3867983bad6e5d
Dan Tiberi <danny.tiberi@gmail.com>
2023-03-30 16:32:48 -0500

    Import mat icons

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { GreetingComponent } from './greeting/greeting.component';
 import { IntroductionComponent } from './introduction/introduction.component';
+import { MatIconModule } from '@angular/material/icon';
 
 const exportList = [HomeComponent, GreetingComponent, IntroductionComponent];
 
 @NgModule({
   declarations:  exportList,
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule
   ],
   exports: exportList
 })
