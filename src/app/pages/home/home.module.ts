667294b6138eb57db6e49b0916b7416aeb1b8594
Dan Tiberi <danny.tiberi@gmail.com>
2023-03-30 16:24:16 -0500

    Generated introduction component.

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { GreetingComponent } from './greeting/greeting.component';
+import { IntroductionComponent } from './introduction/introduction.component';
 
-const exportList = [HomeComponent, GreetingComponent];
+const exportList = [HomeComponent, GreetingComponent, IntroductionComponent];
 
 @NgModule({
   declarations:  exportList,
