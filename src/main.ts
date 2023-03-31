82547cc694d2e52d0d234c3314c1c14000e12f24
Dan Tiberi <danny.tiberi@gmail.com>
2023-03-31 10:35:58 -0500

    Fix mobile horizonal view

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+window.screen.orientation.lock("portrait");
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
