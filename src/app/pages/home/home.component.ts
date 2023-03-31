800a9e12f64d89b4a6613cd84c13f11c0603ed28
Dan Tiberi <danny.tiberi@gmail.com>
2023-03-31 10:05:51 -0500

    Change tab title.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
   showEmail = false;
+
+  constructor(private title:Title) {
+    
+  }
+
+  ngOnInit(): void {
+    this.title.setTitle("Dan Tiberi - Home")
+  }
 
   emailMe(){
     location.href = "mailto:dan.tiberi@protonmail.com";
