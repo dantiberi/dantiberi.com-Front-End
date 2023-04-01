ff0a1686dc4e2c7e01c3b54ad3e4b0145877c04c
Dan Tiberi <danny.tiberi@gmail.com>
2023-03-31 22:49:07 -0500

    Weather card foundations

--- src/app/misc-components/weather/weather.component.ts
+++ src/app/misc-components/weather/weather.component.ts
@@ -13,11 +13,25 @@ export class WeatherComponent implements OnInit{
 
   daysOfWeek: string[] = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
 
+  relativeDaysOfWeek: string[] = []; //Similar to daysOfWeek but made to start with today.
+
+  today = new Date();
+  todayIndex: number= this.today.getDay(); //Day of the week as index 0 - 6, Sunday = 0.
+
   constructor(private weatherService: WeatherService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {  
+    //Fill relativeDaysOfWeek
+    let idx = this.todayIndex;
+    for (let i = 0; i < 7; i++) {
+      if(idx > 6){
+        idx = 0;
+      }
+      this.relativeDaysOfWeek.push(this.daysOfWeek[idx]);
 
-    console.log("Fetching weather...");
+      idx++;
+    }
+
     //Immediately-invoked / Anonymous Async Function
     (async () => {
       this.currentWeather = await this.weatherService.fetchWeather();
