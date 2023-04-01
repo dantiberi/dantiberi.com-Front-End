6a8bf6dc7b01481dd19936c06d76e359c8404e2e
Dan Tiberi <danny.tiberi@gmail.com>
2023-03-31 21:46:09 -0500

    Generate weather component + service

---
+++ src/app/misc-components/weather/weather.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather',
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.scss']
+})
+export class WeatherComponent implements OnInit{
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
