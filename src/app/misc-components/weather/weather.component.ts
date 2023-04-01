0e4eda5ccd9d5351a6acf0e3b455f7c0f9fb8b5e
Dan Tiberi <danny.tiberi@gmail.com>
2023-03-31 22:26:48 -0500

    Fetch weather logic

--- src/app/misc-components/weather/weather.component.ts
+++ src/app/misc-components/weather/weather.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { WeatherData } from './weather-interfaces';
+import { WeatherService } from './weather.service';
 
 @Component({
   selector: 'app-weather',
@@ -6,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather.component.scss']
 })
 export class WeatherComponent implements OnInit{
-  
+
+  currentWeather: WeatherData | undefined;
+
+  constructor(private weatherService: WeatherService) { }
+
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+
+    console.log("Fetching weather...");
+    //Immediately-invoked / Anonymous Async Function
+    (async () => {
+      this.currentWeather = await this.weatherService.fetchWeather();
+    })().then(() => {
+      console.log(this.currentWeather);
+    });
   }
 
 }
+
