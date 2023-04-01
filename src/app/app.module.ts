c8e3c8a548d09f7a15aaee34938561fdd5f9a5cf
Dan Tiberi <danny.tiberi@gmail.com>
2023-03-31 21:39:13 -0500

    Moved banner to new dir.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,10 @@ import { HomeModule } from './pages/home/home.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BannerComponent } from './banner/banner.component';
-import { DarkModeToggleComponent } from './banner/dark-mode-toggle/dark-mode-toggle.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { BannerComponent } from './misc-components/banner/banner.component';
+import { DarkModeToggleComponent } from './misc-components/banner/dark-mode-toggle/dark-mode-toggle.component';
 
 @NgModule({
   declarations: [
