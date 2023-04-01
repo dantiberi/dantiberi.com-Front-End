d9f62ba6acc701c4ec834ca4112e1b8099ec9514
Dan Tiberi <danny.tiberi@gmail.com>
2023-03-31 22:28:27 -0500

    Update home.module.ts

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -6,11 +6,13 @@ import { IntroductionComponent } from './introduction/introduction.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { BrowserModule, Title } from '@angular/platform-browser';
+import { WeatherComponent } from 'src/app/misc-components/weather/weather.component';
 
-const exportList = [HomeComponent, GreetingComponent, IntroductionComponent];
+const exportList = [HomeComponent, GreetingComponent, IntroductionComponent, WeatherComponent];
 
 @NgModule({
-  declarations:  exportList,
+  declarations: exportList,
+
   imports: [
     CommonModule,
     MatIconModule,
